2bc81b2c5ef67c6e730943a327b7b4eba85c0028
Christian Barentine <cbarentine@gmail.com>
2015-08-10 14:36:37 -0500

    Added second room, unity fps controller, and door script that registers clicks

---
+++ Assets/scripts/doorRoomChange.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+// Whenever a door is clicked on, doorRoomChange will fade to black, move the player to the destination room,
+// place them in the same position they were relative to the original room, and fade in.
+
+var destinationRoom : GameObject;
+
+function Start () {
+	// prevent the script from activating if no destination room exists
+	if (destinationRoom == null) {this.enabled = false;}
+	
+}
+
+function OnMouseOver() {
+	if (Input.GetMouseButtonUp(0)){
+		// if the door has been clicked on, and the mouse is over the door, change room.
+		Debug.Log("going to the next room: " + destinationRoom);
+	}
+}
+
+function ChangeRoom(player : GameObject, destination : GameObject) {
+	var relativePosition = player.transform.position - transform.position;
+}
