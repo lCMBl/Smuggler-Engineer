dd34a90cf37513c06d1b0bf95ac225a10f4f6f34
Christian Barentine <cbarentine@gmail.com>
2015-09-14 11:05:11 -0500

    Added maze repair pointer that follows mouse, moved maze to front of camera.

--- Assets/scripts/repair maze/mazeMouseFollow.js
+++ Assets/scripts/repair maze/mazeMouseFollow.js
@@ -1,9 +1,22 @@
 ﻿#pragma strict
 
-function Start () {
+// code taken from : http://answers.unity3d.com/questions/406636/3d-object-attach-to-mouse-1.html
+ 
+public var distance : float = 5;
+ // need to change distance so that sphere doesn't move in an arc around the camera
+ 
 
+function Update() 
+{
+    ObjectFollowCursor();
 }
 
-function Update () {
-
+function ObjectFollowCursor() 
+{
+//	 var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//	 var point : Vector3 = ray.origin + Vector3(0,0,distance);//(ray.direction * distance);
+	 //Debug.Log( "World point " + point );
+	 var point : Vector3 = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3(0,0,distance)); 
+	 
+	 transform.position = point;
 }
