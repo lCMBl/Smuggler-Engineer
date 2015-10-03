df556f859a7143841e64dc6ade8b6d8160056f85
Christian Barentine <cbarentine@gmail.com>
2015-10-03 18:31:42 -0500

    Created Status Light and Hyperspace Lever Prefabs

---
+++ Assets/scripts/StatusLightFlicker.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+// slowly blicks the attacked light on and off
+// only use with point lights
+
+var blinksPerSecond : float = 0.75;
+var rangePercentage : float = 0.8;
+private var lightOn : boolean = false;
+private var attachedLight : Light;
+private var maxRange : float;
+private var rangeAmplitude : float;
+
+
+function Start () {
+	attachedLight = gameObject.GetComponentInChildren(Light);
+	
+	if (attachedLight == null) {
+		this.enabled = false;
+	} else {
+		maxRange = attachedLight.range;
+		rangeAmplitude = maxRange * rangePercentage;
+		DisableLight();
+	}
+}
+
+function Update () {
+	if (lightOn) {
+		attachedLight.range = maxRange - Mathf.Abs( rangeAmplitude * Mathf.Sin(blinksPerSecond * Time.time));
+	}
+}
+
+
+function DisableLight() {
+	lightOn = false;
+	attachedLight.enabled = false;
+}
+
+function EnableLight() {
+	lightOn = true;
+	attachedLight.enabled = true;
+}
